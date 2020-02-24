0599ef2a686c65db00341f74b5f214bd66d40146
miquelcvcvcv <61110942+miquelcvcvcv@users.noreply.github.com>
2020-02-24 03:44:08 +0100

    mejora algoritmo busqueda y terminado punto limite km. falta validar

---
+++ arvato-candidates-spring-boot/src/main/java/de/arvato/geo/domain/Boolfloat.java
@@ -0,0 +1,29 @@
+package de.arvato.geo.domain;
+
+
+
+public class Boolfloat {
+	private float i;
+	 private boolean b;
+	 
+ public float getI() {
+		return i;
+	}
+	public void setI(float i) {
+		this.i = i;
+	}
+	public boolean isB() {
+		return b;
+	}
+	public void setB(boolean b) {
+		this.b = b;
+	}
+
+	public Boolfloat() {
+		i=0;
+		b=false;
+	}
+ 
+	
+	
+}
---
+++ arvato-candidates-spring-boot/src/main/java/de/arvato/geo/domain/Stringfloat.java
@@ -0,0 +1,24 @@
+package de.arvato.geo.domain;
+
+public class Stringfloat {
+	String s;
+	float i;
+
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+	public float getI() {
+		return i;
+	}
+	public void setI(float i) {
+		this.i = i;
+	}
+	public Stringfloat() {
+		s=new String();
+		i=0;
+	}
+ 
+}
